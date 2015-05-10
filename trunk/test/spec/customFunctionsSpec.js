4875b16d28bce14487dfde5ee8fa7d16c04600d8
Cash Sun <mr.cashsun@gmail.com>
2015-05-10 01:10:01 +0100

    cash: custom functions definitions and tests.

--- trunk/test/spec/customFunctionsSpec.js
+++ trunk/test/spec/customFunctionsSpec.js
@@ -6,6 +6,8 @@ require('./../utils/specUtils');
 describe('customFunctions', function(){
   it('generateDate works', function(done){
     var line = 'generateDate(20-may-2015, 5, y) oh yeah';
+
+
     customFunctions.evaluate([line]).should.eventually.equal('\"20-May-2020\" oh yeah')
     .notify(done);
   });
